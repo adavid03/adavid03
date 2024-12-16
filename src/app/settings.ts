3da0b02c426973528d083a113c8a032993bce433
Alexandre David <xanderbrunet@student.lccc.wy.edu>
2024-12-15 18:03:52 -0700

    Initial commit, added settings.ts

---
+++ src/app/settings.ts
@@ -0,0 +1,44 @@
+const settings = {
+  siteTitle: "Alexandre David",
+  siteDescription:
+    "A computer science and research student at the Laramie County Community College, Wyoming.",
+  pages: {
+    home: {
+      title: "Home",
+      description: "A computer science and research student at the Laramie County Community College, Wyoming.",
+      colors: {
+        navbar: "#ffffff"
+      },
+    },
+    about: {
+      title: "About Me",
+      description: "Discover more about me and my journey.",
+      colors: {
+        navbar: "#DC143C"
+      },
+    },
+    projects: {
+      title: "Projects",
+      description: "Explore my projects throughout the years.",
+      colors: {
+        navbar: "#FFC300"
+      },
+    },
+    research:{
+      title: "Research",
+      description: "Explore my research and publications.",
+      colors: {
+        navbar: "#00A36C"
+      },
+    },
+    contact: {
+      title: "Contact",
+      description: "Get in touch with me.",
+      colors: {
+        navbar: "#1434A4"
+      },
+    },
+  },
+};
+
+export default settings;
